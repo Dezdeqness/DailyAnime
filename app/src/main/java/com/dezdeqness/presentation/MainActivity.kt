412185b1ae6a687a442cebd0dcb3271b96e483b6
Danyl Brahin <inicerer141@gmail.com>
2022-10-24 19:50:02 +0300

    [SHKMR-13] Fix bug with toolbar navigation

--- app/src/main/java/com/dezdeqness/presentation/MainActivity.kt
+++ app/src/main/java/com/dezdeqness/presentation/MainActivity.kt
@@ -3,10 +3,7 @@ package com.dezdeqness.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.dezdeqness.R
 import com.dezdeqness.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,16 +17,7 @@ class MainActivity : AppCompatActivity() {
 
         val fragment = supportFragmentManager.findFragmentById(binding.container.id) as NavHostFragment
         val navController = fragment.navController
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.user_preferences,
-                R.id.calendar,
-                R.id.current,
-                R.id.profile,
-                R.id.search,
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+
         binding.navigation.setupWithNavController(navController)
     }
 }
