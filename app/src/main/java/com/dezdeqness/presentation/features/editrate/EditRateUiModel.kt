0ccbfbe3131c7872318420e267cd78d48943604e
Danyl Brahin <inicerer141@gmail.com>
2023-02-18 19:26:12 +0200

    [SHKMR-15] Change parcel package

--- app/src/main/java/com/dezdeqness/presentation/features/editrate/EditRateUiModel.kt
+++ app/src/main/java/com/dezdeqness/presentation/features/editrate/EditRateUiModel.kt
@@ -1,7 +1,7 @@
 package com.dezdeqness.presentation.features.editrate
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class EditRateUiModel(
