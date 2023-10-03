5cedba6022d7bbbd22d9f63eb15e7b98252ece7f
Danyl Brahin <inicerer141@gmail.com>
2023-10-03 22:21:01 +0300

    [SHKMR-27] Add converter

---
+++ core/src/main/kotlin/com/dezdeqness/core/converter/TimestampConverter.kt
@@ -0,0 +1,26 @@
+package com.dezdeqness.core.converter
+
+import java.text.SimpleDateFormat
+import java.util.Locale
+import javax.inject.Inject
+
+class TimestampConverter @Inject constructor() {
+
+    private val timeStampFormatterWithTime =
+        SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ", Locale.getDefault())
+
+    private val timeStampFormatter =
+        SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+
+    fun convertToTimeStampWithTime(value: String?): Long {
+        return timeStampFormatterWithTime.parse(value.orEmpty())?.time ?: 0L
+    }
+
+    fun convertToTimeStamp(value: String?): Long {
+        if (value.isNullOrEmpty()) {
+            return 0L
+        }
+        return timeStampFormatter.parse(value)?.time ?: 0L
+    }
+
+}
