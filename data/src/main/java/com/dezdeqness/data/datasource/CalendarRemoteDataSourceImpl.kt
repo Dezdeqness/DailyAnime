ba8807fb60fc30f3d4fe05254cf491e3a9156ed6
Danyl Brahin <inicerer141@gmail.com>
2024-09-15 20:18:44 +0300

    [DA-18] Add Lazy for fix issue when config is not loaded yet, but retrofit has already created with empty or invalid data

--- data/src/main/java/com/dezdeqness/data/datasource/CalendarRemoteDataSourceImpl.kt
+++ data/src/main/java/com/dezdeqness/data/datasource/CalendarRemoteDataSourceImpl.kt
@@ -1,19 +1,19 @@
 package com.dezdeqness.data.datasource
 
 import com.dezdeqness.data.CalendarApiService
-import com.dezdeqness.data.core.ApiException
 import com.dezdeqness.data.core.BaseDataSource
 import com.dezdeqness.data.core.createApiException
 import com.dezdeqness.data.mapper.CalendarMapper
+import dagger.Lazy
 import javax.inject.Inject
 
 class CalendarRemoteDataSourceImpl @Inject constructor(
-    private val calendarApiService: CalendarApiService,
+    private val calendarApiService: Lazy<CalendarApiService>,
     private val calendarMapper: CalendarMapper,
 ) : CalendarRemoteDataSource, BaseDataSource() {
 
     override fun getCalendar() = tryWithCatch {
-        val response = calendarApiService.getCalendar().execute()
+        val response = calendarApiService.get().getCalendar().execute()
 
         val responseBody = response.body()
         if (response.isSuccessful && responseBody != null) {
