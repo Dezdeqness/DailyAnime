ed44b1eb38723d56d98116e6081c36e600f76ad6
Danyl Brahin <inicerer141@gmail.com>
2024-10-15 22:23:23 +0300

    [DA-50] Create store, reducer, and other stuff

--- app/src/main/java/com/dezdeqness/di/modules/ScreenshotsModule.kt
+++ app/src/main/java/com/dezdeqness/di/modules/ScreenshotsModule.kt
@@ -1,19 +1,55 @@
 package com.dezdeqness.di.modules
 
 import androidx.lifecycle.ViewModel
+import com.dezdeqness.core.CoroutineDispatcherProvider
+import com.dezdeqness.data.core.AppLogger
 import com.dezdeqness.di.ViewModelKey
+import com.dezdeqness.di.subcomponents.ScreenshotsArgsModule
+import com.dezdeqness.pod.core.scope.plantScope
+import com.dezdeqness.pod.core.store.internal.PlantStore
 import com.dezdeqness.presentation.features.screenshotsviewer.ScreenshotsViewModel
+import com.dezdeqness.presentation.features.screenshotsviewer.store.ScreenshotsNamespace.Effect
+import com.dezdeqness.presentation.features.screenshotsviewer.store.ScreenshotsNamespace.Event
+import com.dezdeqness.presentation.features.screenshotsviewer.store.ScreenshotsNamespace.State
+import com.dezdeqness.presentation.features.screenshotsviewer.store.screenshotReducer
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.multibindings.IntoMap
+import javax.inject.Named
 
+private const val KEY = "ScreenshotStore"
 
-@Module
+@Module(includes = [ScreenshotsArgsModule::class])
 abstract class ScreenshotsModule {
 
     @Binds
     @IntoMap
     @ViewModelKey(ScreenshotsViewModel::class)
     abstract fun bindScreenshotsViewModel(viewModel: ScreenshotsViewModel): ViewModel
+
+    companion object {
+        @Provides
+        fun provideScreenshotStore(
+            @Named("index") index: Int,
+            @Named("screenshots") screenshots: List<String>,
+            coroutineDispatcherProvider: CoroutineDispatcherProvider,
+            appLogger: AppLogger,
+        ): PlantStore<Event, State, Effect> =
+            PlantStore(
+                state = State(
+                    screenshotsList = screenshots,
+                    index = index,
+                ),
+                reducer = screenshotReducer,
+                storeScope = plantScope(
+                    key = KEY,
+                    dispatcher = coroutineDispatcherProvider.io(),
+                    onError = { throwable ->
+                        appLogger.logInfo(tag = KEY, throwable = throwable)
+                    }
+                )
+            )
+    }
 
 }
