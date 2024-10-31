e2a3a7c0c8dbb111d75f6081ea9e2c86950c3865
Danyl Brahin <inicerer141@gmail.com>
2024-10-31 20:38:34 +0200

    [DA-42] Implement serach field

---
+++ app/src/main/java/com/dezdeqness/presentation/features/personallist/composable/PersonalListSearch.kt
@@ -0,0 +1,84 @@
+package com.dezdeqness.presentation.features.personallist.composable
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dezdeqness.R
+import com.dezdeqness.core.ui.SearchTextField
+import com.dezdeqness.core.ui.rememberSearchState
+import com.dezdeqness.core.ui.theme.AppTheme
+
+@Composable
+fun PersonalListSearch(
+    modifier: Modifier = Modifier,
+    onQueryChanged: (String) -> Unit,
+) {
+    val searchState = rememberSearchState()
+
+    LaunchedEffect(searchState.query) {
+        onQueryChanged(searchState.query)
+    }
+
+    SearchTextField(
+        modifier = modifier,
+        state = searchState,
+        onQueryChanged = onQueryChanged,
+        placeholder = {
+            Text(
+                text = stringResource(id = R.string.personal_list_search_placeholder_hint),
+                style = AppTheme.typography.titleMedium.copy(
+                    fontSize = 18.sp,
+                    color = AppTheme.colors.textPrimary.copy(alpha = 0.8f),
+                ),
+            )
+        },
+        trailingIcon = {
+            IconButton(
+                onClick = {
+                    searchState.query = ""
+                    onQueryChanged(searchState.query)
+                },
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_close),
+                    contentDescription = null,
+                    tint = AppTheme.colors.onSecondary
+                )
+            }
+        },
+        leadingIcon = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_search),
+                contentDescription = null,
+                tint = AppTheme.colors.onSecondary
+            )
+        },
+    )
+
+}
+
+@Preview
+@Composable
+fun PersonaListSearchPreview() {
+    AppTheme {
+        Box(
+            modifier = Modifier.background(AppTheme.colors.onPrimary)
+        ) {
+            PersonalListSearch(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                onQueryChanged = {},
+            )
+        }
+    }
+}
