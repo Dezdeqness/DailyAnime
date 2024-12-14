68d6bf06d8ca0fa78b75782f7d0981aa3fa5a8bf
Danyl Brahin <inicerer141@gmail.com>
2024-12-14 17:30:04 +0200

    [DA-67] Map error from API

--- data/src/main/java/com/dezdeqness/data/core/BaseDataSource.kt
+++ data/src/main/java/com/dezdeqness/data/core/BaseDataSource.kt
@@ -1,5 +1,7 @@
 package com.dezdeqness.data.core
 
+import com.apollographql.apollo.api.ApolloResponse
+import com.apollographql.apollo.api.Operation
 import com.dezdeqness.domain.mapper.ErrorMapper
 import retrofit2.Response
 import javax.inject.Inject
@@ -15,7 +17,22 @@ abstract class BaseDataSource {
         Result.failure(errorMapper.map(exception))
     }
 
+    suspend fun <T> tryWithCatchSuspend(block: suspend () -> Result<T>) = try {
+        block()
+    } catch (exception: Throwable) {
+        Result.failure(errorMapper.map(exception))
+    }
+
+
 }
 
 fun <T> Response<T>.createApiException() =
     ApiException(code(), errorBody()?.string() ?: "No mappable error")
+
+fun <T : Operation.Data> ApolloResponse<T>.createGraphqlException() =
+    GraphqlException(
+        operation.name(),
+        errors?.toString()
+            ?: exception?.toString()
+            ?: "No mappable error",
+    )
