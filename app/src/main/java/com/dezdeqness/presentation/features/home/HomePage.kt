68edf9fe3e74fa077dbcc1afaf3de222b63ddcb1
Danyl Brahin <inicerer141@gmail.com>
2025-01-05 22:35:53 +0200

    [DA-49] Apply skeleton loading to home screen

--- app/src/main/java/com/dezdeqness/presentation/features/home/HomePage.kt
+++ app/src/main/java/com/dezdeqness/presentation/features/home/HomePage.kt
@@ -5,11 +5,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import com.dezdeqness.R
 import com.dezdeqness.presentation.features.home.composable.HomeBanner
 import com.dezdeqness.presentation.features.home.composable.HomeSection
+import com.dezdeqness.presentation.features.home.composable.SectionStatus
+import com.dezdeqness.presentation.features.home.composable.ShimmerHomeLoading
 
 @Composable
 fun HomePage(
@@ -24,10 +27,19 @@ fun HomePage(
     val sections = state.sectionsState.sections
     val authorizedState = state.authorizedState
 
+    val isLoadingVisible = remember(sections) {
+        sections.map { it.status }.any { it == SectionStatus.Initial || it == SectionStatus.Loading }
+    }
+
+    val isEmptyContent = remember(sections) {
+        sections.map { it.items }.all { it.isEmpty() }
+    }
+
     LazyColumn(
         modifier = modifier
             .fillMaxSize()
             .background(colorResource(id = R.color.background_tint)),
+        userScrollEnabled = isLoadingVisible.not(),
     ) {
         item {
             HomeBanner(
@@ -35,17 +47,23 @@ fun HomePage(
             )
         }
 
-        items(
-            count = sections.size,
-            key = { index -> sections[index].toString() },
-        ) { index ->
-            val section = sections[index]
-            HomeSection(
-                title = section.title,
-                items = section.items,
-                status = section.status,
-                onActionReceive = actions::onActionReceived,
-            )
+        if (isLoadingVisible && isEmptyContent) {
+            item {
+                ShimmerHomeLoading()
+            }
+        } else {
+            items(
+                count = sections.size,
+                key = { index -> sections[index].toString() },
+            ) { index ->
+                val section = sections[index]
+                HomeSection(
+                    title = section.title,
+                    items = section.items,
+                    status = section.status,
+                    onActionReceive = actions::onActionReceived,
+                )
+            }
         }
     }
 
---
+++ app/src/main/java/com/dezdeqness/presentation/features/home/composable/ShimmerHomeLoading.kt
@@ -0,0 +1,89 @@
+package com.dezdeqness.presentation.features.home.composable
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.dezdeqness.core.ui.shimmer
+
+@Composable
+fun ShimmerHomeLoading(
+    modifier: Modifier = Modifier,
+    times: Int = 3,
+) {
+    Column(
+        modifier = modifier
+    ) {
+        repeat(times) { _ ->
+            ShimmerHomeHeader(
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+            ShimmerHomeItem()
+        }
+    }
+}
+
+@Composable
+private fun ShimmerHomeHeader(
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .padding(vertical = 16.dp)
+            .width(100.dp)
+            .height(20.dp)
+            .shimmer()
+    )
+}
+
+@Composable
+private fun ShimmerHomeItem(
+    modifier: Modifier = Modifier,
+    times: Int = 5,
+) {
+    Box(modifier = modifier) {
+        LazyRow (
+            contentPadding = PaddingValues(horizontal = 16.dp),
+        ) {
+            items(count = times) {
+                Column(
+                    modifier = Modifier.padding(end = 8.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .width(100.dp)
+                            .height(120.dp)
+                            .shimmer()
+                    )
+                    Spacer(modifier = Modifier.size(8.dp))
+
+                    Box(
+                        modifier = Modifier
+                            .width(80.dp)
+                            .height(14.dp)
+                            .shimmer()
+                    )
+
+                    Spacer(modifier = Modifier.size(4.dp))
+
+                    Box(
+                        modifier = Modifier
+                            .width(80.dp)
+                            .height(14.dp)
+                            .shimmer()
+                    )
+                }
+            }
+        }
+    }
+}
