c33b9be33962ffc5f66a4fb0588c6fd955a9b36b
Danyl Brahin <inicerer141@gmail.com>
2025-03-11 23:05:13 +0200

    [DA-79] Add new settings to datastore, refactor ribbon mapper, create status provider

---
+++ data/src/main/java/com/dezdeqness/data/provider/StatusesProvider.kt
@@ -0,0 +1,55 @@
+package com.dezdeqness.data.provider
+
+import com.dezdeqness.domain.model.StatusEntity
+import javax.inject.Inject
+
+class StatusesProvider @Inject constructor() {
+
+    fun getStatuses(): List<StatusEntity> {
+        return listOf(
+            StatusEntity(
+                id = 0,
+                groupedId = "planned",
+                name = "planned",
+                size = 0,
+                type = "Anime"
+            ),
+            StatusEntity(
+                id = 1,
+                groupedId = "watching",
+                name = "watching",
+                size = 0,
+                type = "Anime"
+            ),
+            StatusEntity(
+                id = 9,
+                groupedId = "rewatching",
+                name = "rewatching",
+                size = 0,
+                type = "Anime"
+            ),
+            StatusEntity(
+                id = 2,
+                groupedId = "completed",
+                name = "completed",
+                size = 0,
+                type = "Anime"
+            ),
+            StatusEntity(
+                id = 3,
+                groupedId = "on_hold",
+                name = "on_hold",
+                size = 0,
+                type = "Anime"
+            ),
+            StatusEntity(
+                id = 4,
+                groupedId = "dropped",
+                name = "dropped",
+                size = 0,
+                type = "Anime"
+            ),
+        )
+    }
+
+}
