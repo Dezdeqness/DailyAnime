e69d79f3ca10529ab07fec0dd7c5e1cfeeaef36c
Danyl Brahin <inicerer141@gmail.com>
2025-03-16 16:18:20 +0200

    [DA-78] Add analytics manager with events

---
+++ data/src/main/java/com/dezdeqness/data/analytics/impl/AnalyticsManagerImpl.kt
@@ -0,0 +1,129 @@
+package com.dezdeqness.data.analytics.impl
+
+import android.os.Build
+import android.os.Bundle
+import androidx.core.os.bundleOf
+import com.dezdeqness.data.analytics.AnalyticsManager
+import com.dezdeqness.data.analytics.model.AnalyticsEvent
+import com.dezdeqness.data.analytics.model.AnalyticsScreenName
+import com.dezdeqness.domain.repository.AccountRepository
+import com.google.firebase.analytics.FirebaseAnalytics
+
+class AnalyticsManagerImpl(
+    private val firebaseAnalytics: FirebaseAnalytics,
+    private val accountRepository: AccountRepository,
+) : AnalyticsManager {
+
+    private val deviceInfo: Bundle by lazy {
+        bundleOf(
+            "device_model" to Build.MODEL,
+            "device_name" to Build.DEVICE,
+            "manufacturer" to Build.MANUFACTURER,
+            "os_version" to Build.VERSION.RELEASE,
+            "api_level" to Build.VERSION.SDK_INT.toString(),
+        )
+    }
+
+    override fun personalListTracked() {
+        val data = bundleOf().apply {
+            putUserId()
+            putAll(deviceInfo)
+        }
+
+        val event = AnalyticsEvent(screenName = AnalyticsScreenName.PersonalList, data = data)
+        logEvent(event)
+    }
+
+    override fun homeTracked() {
+        val data = bundleOf().apply {
+            putUserId()
+            putAll(deviceInfo)
+        }
+
+        val event = AnalyticsEvent(screenName = AnalyticsScreenName.Home, data = data)
+        logEvent(event)
+    }
+
+    override fun calendarTracked(isFromHome: Boolean) {
+        val data = bundleOf().apply {
+            putUserId()
+            putAll(deviceInfo)
+            putBoolean(FROM_HOME, isFromHome)
+        }
+
+        val event = AnalyticsEvent(screenName = AnalyticsScreenName.Calendar, data = data)
+        logEvent(event)
+    }
+
+    override fun searchTracked() {
+        val data = bundleOf().apply {
+            putUserId()
+            putAll(deviceInfo)
+        }
+
+        val event = AnalyticsEvent(screenName = AnalyticsScreenName.Search, data = data)
+        logEvent(event)
+    }
+
+    override fun profileTracked() {
+        val data = bundleOf().apply {
+            putUserId()
+            putAll(deviceInfo)
+        }
+
+        val event = AnalyticsEvent(screenName = AnalyticsScreenName.Profile, data = data)
+        logEvent(event)
+    }
+
+    override fun detailsTracked(id: String, title: String) {
+        val data = bundleOf().apply {
+            putUserId()
+            putAll(deviceInfo)
+            putString(ANIME_ID, id)
+            putString(ANIME_TITLE, title)
+        }
+
+        val event = AnalyticsEvent(screenName = AnalyticsScreenName.Details, data = data)
+        logEvent(event)
+    }
+
+    override fun settingsTracked() {
+        val data = bundleOf().apply {
+            putUserId()
+            putAll(deviceInfo)
+        }
+
+        val event = AnalyticsEvent(screenName = AnalyticsScreenName.Settings, data = data)
+        logEvent(event)
+    }
+
+    override fun authTracked(isLogin: Boolean) {
+        val data = bundleOf().apply {
+            putUserId()
+            putAll(deviceInfo)
+            putBoolean(LOGIN_FLOW, isLogin)
+        }
+
+        val event = AnalyticsEvent(screenName = AnalyticsScreenName.Auth, data = data)
+        logEvent(event)
+    }
+
+    private fun Bundle.putUserId() = this.apply {
+        putString(USER_ID, getUserId())
+    }
+
+    private fun logEvent(event: AnalyticsEvent) {
+        firebaseAnalytics.logEvent(event.screenName.name, event.data)
+    }
+
+    private fun getUserId() = accountRepository.getProfileLocal()?.id?.toString() ?: ANONYMOUS_USER
+
+    companion object {
+        private const val ANONYMOUS_USER = "anonymous"
+        private const val USER_ID = "userId"
+        private const val FROM_HOME = "fromHome"
+        private const val LOGIN_FLOW = "loginFlow"
+        private const val ANIME_ID = "animeId"
+        private const val ANIME_TITLE = "animeTitle"
+    }
+}
---
+++ data/src/main/java/com/dezdeqness/data/analytics/model/AnalyticsScreenName.kt
@@ -0,0 +1,12 @@
+package com.dezdeqness.data.analytics.model
+
+enum class AnalyticsScreenName {
+    PersonalList,
+    Home,
+    Calendar,
+    Search,
+    Profile,
+    Details,
+    Settings,
+    Auth,
+}
