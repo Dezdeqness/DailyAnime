b93f196b1c3ba2e01bcfb5671a1efcb7403b3677
Danyl Brahin <inicerer141@gmail.com>
2025-03-19 22:27:40 +0200

    [DA-43] Fix bug with crypto lib

--- data/src/main/java/com/dezdeqness/data/serializer/TokenSerializer.kt
+++ data/src/main/java/com/dezdeqness/data/serializer/TokenSerializer.kt
@@ -1,22 +1,31 @@
 package com.dezdeqness.data.serializer
 
+import android.content.Context
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
 import com.dezdeqness.data.TokenEntityProto
 import com.google.crypto.tink.Aead
-import com.google.crypto.tink.KeysetHandle
 import com.google.crypto.tink.aead.AeadConfig
 import com.google.crypto.tink.aead.AeadKeyTemplates
+import com.google.crypto.tink.integration.android.AndroidKeysetManager
 import com.google.crypto.tink.shaded.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
+import javax.inject.Inject
 
-object TokenSerializer : Serializer<TokenEntityProto> {
-    private val aead: Aead
+class TokenSerializer @Inject constructor(context: Context) : Serializer<TokenEntityProto> {
+    private var aead: Aead
 
     init {
         AeadConfig.register()
-        val keysetHandle = KeysetHandle.generateNew(AeadKeyTemplates.AES256_GCM)
+
+        val keysetHandle = AndroidKeysetManager.Builder()
+            .withSharedPref(context, KEYSET_NAME, PREFERENCE_FILE_NAME)
+            .withKeyTemplate(AeadKeyTemplates.AES256_GCM)
+            .withMasterKeyUri(MASTER_KEY_URI)
+            .build()
+            .keysetHandle
+
         aead = keysetHandle.getPrimitive(Aead::class.java)
     }
 
@@ -29,11 +38,19 @@ object TokenSerializer : Serializer<TokenEntityProto> {
             TokenEntityProto.parseFrom(decryptedBytes)
         } catch (e: InvalidProtocolBufferException) {
             throw CorruptionException("Cannot read TokenEntityProto", e)
+        } catch (e: Exception) {
+            throw CorruptionException("Decryption failed", e)
         }
     }
 
     override suspend fun writeTo(t: TokenEntityProto, output: OutputStream) {
         val encryptedBytes = aead.encrypt(t.toByteArray(), null)
         output.write(encryptedBytes)
+    }
+
+    companion object {
+        private const val KEYSET_NAME = "master_keyset"
+        private const val PREFERENCE_FILE_NAME = "secure_prefs"
+        private const val MASTER_KEY_URI = "android-keystore://master_key"
     }
 }
