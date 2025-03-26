89d1db5cd8f5bf7f994784d664383f5881cf55dc
Danyl Brahin <inicerer141@gmail.com>
2025-03-26 21:03:25 +0200

    [DA-70] Add interactive generic error & empty states

--- app/src/main/java/com/dezdeqness/core/ui/GeneralEmpty.kt
+++ app/src/main/java/com/dezdeqness/core/ui/GeneralEmpty.kt
@@ -1,14 +1,18 @@
 package com.dezdeqness.core.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -17,20 +21,36 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.dezdeqness.R
 import com.dezdeqness.core.ui.theme.AppTheme
 
 @Composable
 fun GeneralEmpty(modifier: Modifier = Modifier) {
-    Box(modifier = modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        val compositionLoading by rememberLottieComposition(
+            LottieCompositionSpec.RawRes(R.raw.empty)
+        )
+
+        LottieAnimation(
+            composition = compositionLoading,
+            iterations = LottieConstants.IterateForever,
+            modifier = Modifier.height(120.dp)
+        )
+
         Text(
             text = stringResource(id = R.string.search_empty_state_title),
             textAlign = TextAlign.Center,
-            modifier = Modifier
-                .align(Alignment.Center)
-                .padding(horizontal = 16.dp),
+            modifier = Modifier.padding(horizontal = 16.dp),
             fontSize = 18.sp,
             fontWeight = FontWeight.Bold,
             color = AppTheme.colors.textPrimary,
@@ -43,7 +63,9 @@ fun GeneralEmpty(modifier: Modifier = Modifier) {
 fun AnimeSearchEmptyPreview() {
     AppTheme {
         GeneralEmpty(
-            modifier = Modifier.background(AppTheme.colors.onPrimary).size(300.dp)
+            modifier = Modifier
+                .background(AppTheme.colors.onPrimary)
+                .size(300.dp)
         )
     }
 }
