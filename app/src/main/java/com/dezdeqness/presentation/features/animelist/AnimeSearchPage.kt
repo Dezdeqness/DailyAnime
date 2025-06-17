f01d403c401c1aa50b0b590714fe6b60ff58e5e6
Danyl Brahin <inicerer141@gmail.com>
2025-06-17 22:55:20 +0300

    [DA-100] Add animation when search bar is not showing while scroll down

--- app/src/main/java/com/dezdeqness/presentation/features/animelist/AnimeSearchPage.kt
+++ app/src/main/java/com/dezdeqness/presentation/features/animelist/AnimeSearchPage.kt
@@ -12,9 +12,12 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.pullrefresh.PullRefreshIndicator
 import androidx.compose.material.pullrefresh.pullRefresh
 import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -24,6 +27,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -37,7 +43,7 @@ import com.dezdeqness.presentation.features.animelist.composable.ShimmerSearchLo
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterialApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun AnimeSearchPage(
     modifier: Modifier = Modifier,
@@ -47,6 +53,8 @@ fun AnimeSearchPage(
     isListScrollingFlow: StateFlow<Boolean>,
     actions: AnimeSearchActions,
 ) {
+    val density = LocalDensity.current
+
     val scope = rememberCoroutineScope()
 
     val state by stateFlow.collectAsStateWithLifecycle()
@@ -57,15 +65,32 @@ fun AnimeSearchPage(
 
     val isListScrolling by isListScrollingFlow.collectAsStateWithLifecycle()
 
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    var searchBarHeight by remember { mutableStateOf(TopAppBarDefaults.TopAppBarExpandedHeight) }
+
     Scaffold(
         containerColor = AppTheme.colors.onPrimary,
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            AnimeSearch(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-                onQueryChanged = { query ->
-                    actions.onQueryChanged(query)
-                }
+            TopAppBar(
+                expandedHeight = searchBarHeight,
+                title = {
+                    AnimeSearch(
+                        modifier = Modifier
+                            .padding(vertical = 8.dp)
+                            .padding(end = 16.dp)
+                            .onGloballyPositioned {
+                                searchBarHeight = with(density) { it.size.height.toDp() }
+                            },
+                        onQueryChanged = { query ->
+                            actions.onQueryChanged(query)
+                        }
+                    )
+                },
+                scrollBehavior = scrollBehavior
             )
         },
         floatingActionButton = {
