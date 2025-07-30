97665d219692cbd80b38cafa2e178bcb3c02f815
Danyl Brahin <inicerer141@gmail.com>
2025-07-30 21:58:52 +0300

    [DA-109] Refactor navigation on personal flow

--- app/src/main/java/com/dezdeqness/presentation/features/personallist/PersonalListFragment.kt
+++ app/src/main/java/com/dezdeqness/presentation/features/personallist/PersonalListFragment.kt
@@ -1,109 +1,77 @@
 package com.dezdeqness.presentation.features.personallist
 
-import android.os.Bundle
-import android.view.View
+import android.app.Activity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityOptionsCompat
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.fragment.findNavController
-import com.dezdeqness.core.BaseComposeFragment
-import com.dezdeqness.core.ui.theme.AppTheme
+import androidx.navigation.NavHostController
+import com.dezdeqness.core.utils.collectEvents
 import com.dezdeqness.data.analytics.AnalyticsManager
-import com.dezdeqness.di.AppComponent
+import com.dezdeqness.presentation.Details
 import com.dezdeqness.presentation.action.Action
 import com.dezdeqness.presentation.event.AnimeDetails
 import com.dezdeqness.presentation.event.ConsumableEvent
-import com.dezdeqness.presentation.event.Event
 import com.dezdeqness.presentation.event.EventConsumer
 import com.dezdeqness.presentation.event.NavigateToEditRate
 import com.dezdeqness.presentation.features.userrate.UserRateActivity
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
+@Composable
+fun PersonalListStandalonePage(
+    modifier: Modifier = Modifier,
+    viewModel: PersonalListViewModel,
+    analyticsManager: AnalyticsManager,
+    navController: NavHostController,
+) {
+    val context = LocalContext.current
 
-class PersonalListFragment : BaseComposeFragment() {
-
-    @Inject
-    lateinit var analyticsManager: AnalyticsManager
-
-    private val eventConsumer: EventConsumer by lazy {
+    val eventConsumer = remember {
         EventConsumer(
-            fragment = this,
-            context = this.requireContext(),
+            context = context,
         )
     }
 
-    private val editRateResult = registerForActivityResult(UserRateActivity.UserRate()) { userRate ->
+    val editRateResult = rememberLauncherForActivityResult(UserRateActivity.UserRate()) { userRate ->
         viewModel.onUserRateChanged(userRate)
     }
 
-    private val viewModel: PersonalListViewModel by viewModels(factoryProducer = { viewModelFactory })
+    PersonalListPage(
+        modifier = modifier,
+        stateFlow = viewModel.personalListStateFlow,
+        actions = object : PersonalListActions {
+            override fun onPullDownRefreshed() {
+                viewModel.onPullDownRefreshed()
+            }
 
-    @Composable
-    override fun FragmentContent() {
-        AppTheme {
-            PersonalListPage(
-                stateFlow = viewModel.personalListStateFlow,
-                actions = object : PersonalListActions {
-                    override fun onPullDownRefreshed() {
-                        viewModel.onPullDownRefreshed()
-                    }
+            override fun onScrolled() {
+                viewModel.onScrolled()
+            }
 
-                    override fun onScrolled() {
-                        viewModel.onScrolled()
-                    }
+            override fun onInitialLoad() {
+                viewModel.onInitialLoad()
+            }
 
-                    override fun onInitialLoad() {
-                        viewModel.onInitialLoad()
-                    }
+            override fun onActionReceived(action: Action) {
+                viewModel.onActionReceive(action)
+            }
 
-                    override fun onActionReceived(action: Action) {
-                        viewModel.onActionReceive(action)
-                    }
+            override fun onQueryChanged(query: String) {
+                viewModel.onQueryChanged(query)
+            }
 
-                    override fun onQueryChanged(query: String) {
-                        viewModel.onQueryChanged(query)
-                    }
+            override fun onOrderChanged(order: String) {
+                viewModel.onSortChanged(order)
+            }
 
-                    override fun onOrderChanged(order: String) {
-                        viewModel.onSortChanged(order)
-                    }
-
-                    override fun onRibbonItemSelected(id: String) {
-                        viewModel.onRibbonItemSelected(id)
-                    }
-
-                }
-            )
-        }
-    }
-
-    override fun setupScreenComponent(component: AppComponent) =
-        component
-            .personalListComponent()
-            .create()
-            .inject(this)
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupObservers()
-    }
-
-    private fun setupObservers() {
-        viewLifecycleOwner.lifecycleScope.launch {
-            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.events.collect { event ->
-                    onEvent(event)
-                }
+            override fun onRibbonItemSelected(id: String) {
+                viewModel.onRibbonItemSelected(id)
             }
         }
-    }
+    )
 
-    private fun onEvent(event: Event) {
+    viewModel.events.collectEvents { event ->
         when (event) {
             is NavigateToEditRate -> {
                 editRateResult.launch(
@@ -111,7 +79,7 @@ class PersonalListFragment : BaseComposeFragment() {
                         userRateId = event.rateId,
                         title = event.title,
                     ),
-                    ActivityOptionsCompat.makeSceneTransitionAnimation(requireActivity()),
+                    ActivityOptionsCompat.makeSceneTransitionAnimation(context as Activity),
                 )
             }
 
@@ -120,9 +88,8 @@ class PersonalListFragment : BaseComposeFragment() {
                     id = event.animeId.toString(),
                     title = event.title
                 )
-                findNavController().navigate(
-                        PersonalListFragmentDirections.navigateToAnimeDetails(event.animeId)
-                    )
+
+                navController.navigate(Details(event.animeId))
             }
 
             is ConsumableEvent -> {
