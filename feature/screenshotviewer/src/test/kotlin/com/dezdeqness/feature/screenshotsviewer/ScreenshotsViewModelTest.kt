12c4019fdc3df88af700d10f0f364e7ac7c14771
Danyl Brahin <inicerer141@gmail.com>
2025-09-13 23:16:16 +0300

    [DA-112] Fix tests

--- feature/screenshotviewer/src/test/kotlin/com/dezdeqness/feature/screenshotsviewer/ScreenshotsViewModelTest.kt
+++ feature/screenshotviewer/src/test/kotlin/com/dezdeqness/feature/screenshotsviewer/ScreenshotsViewModelTest.kt
@@ -1,10 +1,9 @@
-package com.dezdeqness.presentation.features.screenshotsviewer
+package com.dezdeqness.feature.screenshotsviewer
 
-import com.dezdeqness.data.core.AppLogger
-import com.dezdeqness.presentation.features.screenshotsviewer.store.ScreenshotsNamespace.Command
-import com.dezdeqness.presentation.features.screenshotsviewer.store.ScreenshotsNamespace.State
-import com.dezdeqness.presentation.features.screenshotsviewer.store.ScreenshotsNamespace.Effect
-import com.dezdeqness.presentation.features.screenshotsviewer.store.ScreenshotsNamespace.Event
+import com.dezdeqness.feature.screenshotsviewer.store.ScreenshotsNamespace.Command
+import com.dezdeqness.feature.screenshotsviewer.store.ScreenshotsNamespace.State
+import com.dezdeqness.feature.screenshotsviewer.store.ScreenshotsNamespace.Effect
+import com.dezdeqness.feature.screenshotsviewer.store.ScreenshotsNamespace.Event
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
@@ -14,15 +13,11 @@ import kotlinx.coroutines.flow.flowOf
 import money.vivid.elmslie.core.store.ElmStore
 import org.junit.Before
 import org.junit.Test
-import utils.TestCoroutineDispatcherProvider
 
 class ScreenshotsViewModelTest {
 
     @MockK(relaxUnitFun = true)
     private lateinit var store: ElmStore<Event, State, Effect, Command>
-
-    @MockK
-    private lateinit var appLogger: AppLogger
 
     private lateinit var viewModel: ScreenshotsViewModel
 
@@ -33,13 +28,8 @@ class ScreenshotsViewModelTest {
         every { store.states } returns MutableStateFlow(State(listOf(), 0))
         every { store.effects } returns flowOf()
 
-        every { appLogger.logInfo(any(), any()) } returns Unit
-        every { appLogger.logInfo(any(), any(), any()) } returns Unit
-
         viewModel = ScreenshotsViewModel(
             store = store,
-            coroutineDispatcherProvider = TestCoroutineDispatcherProvider(),
-            appLogger = appLogger,
         )
     }
 
