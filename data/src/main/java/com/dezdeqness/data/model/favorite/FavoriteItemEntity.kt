43978195164e7f6c568d7a10d8b8b1f4370ace11
Danyl Brahin <inicerer141@gmail.com>
2025-11-03 22:28:10 +0200

    [DA-131] Add API endpoints and models

---
+++ data/src/main/java/com/dezdeqness/data/model/favorite/FavoriteItemEntity.kt
@@ -0,0 +1,16 @@
+package com.dezdeqness.data.model.favorite
+
+import com.squareup.moshi.Json
+
+data class FavoriteItemEntity(
+    @field:Json(name = "id")
+    val id: Long,
+    @field:Json(name = "name")
+    val name: String,
+    @field:Json(name = "russian")
+    val russian: String? = null,
+    @field:Json(name = "image")
+    val image: String? = null,
+    @field:Json(name = "url")
+    val url: String? = null,
+)
