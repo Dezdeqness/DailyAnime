8f24e08bf5c74e4d829adf87300fe124ca93cc91
Danyl Brahin <inicerer141@gmail.com>
2025-11-03 22:27:14 +0200

    [DA-131] Create contract module

---
+++ contract/favourite/src/main/kotlin/com/dezdeqness/contract/favourite/model/FavouriteKind.kt
@@ -0,0 +1,9 @@
+package com.dezdeqness.contract.favourite.model
+
+enum class FavouriteKind(val kind: String) {
+    COMMON("common"),
+    SEYU("seyu"),
+    MANGAKA("mangaka"),
+    PRODUCER("producer"),
+    PERSON("person")
+}
