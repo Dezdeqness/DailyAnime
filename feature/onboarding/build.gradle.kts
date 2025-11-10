0949ff25cb700b2fbfd2069e20d9afe3e01f6504
Danyl Brahin <inicerer141@gmail.com>
2025-11-10 21:11:15 +0200

    [DA-11] Create onboarding feature module

---
+++ feature/onboarding/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.com.dezdeqness.config)
+    alias(libs.plugins.com.dezdeqness.detekt)
+    alias(libs.plugins.com.dezdeqness.compose)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+android {
+    namespace = "com.dezdeqness.feature.onboarding"
+    compileOptions {
+        val javaVersion = JavaVersion.VERSION_21
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
+    }
+
+    buildTypes {
+        create("qa") {}
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    // Dagger
+    implementation(libs.dagger.dagger)
+    ksp(libs.dagger.compilier)
+
+    // Common
+    implementation(project(":common:core-ui"))
+    implementation(project(":common:core"))
+
+    // Unit Testing
+    testImplementation(libs.junit.api)
+    testImplementation(libs.junit.engine)
+    testImplementation(libs.turbine)
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    // Android Testing
+    testImplementation(libs.androidx.test.junit)
+    testImplementation(libs.androidx.test.espresso)
+
+    // Mock
+    testImplementation(libs.mockk.mockk)
+
+    // Collection
+    implementation(libs.androidx.collection)
+
+    implementation(project(":data"))
+    implementation(project(":domain"))
+}
