6b158eeb687c6609591980961b5a29357d04acfc
Danyl Brahin <inicerer141@gmail.com>
2025-11-13 23:02:05 +0200

    [DA-11] Add setting, refactor home genre provider

--- data/src/main/java/com/dezdeqness/data/provider/HomeGenresProvider.kt
+++ data/src/main/java/com/dezdeqness/data/provider/HomeGenresProvider.kt
@@ -1,20 +1,36 @@
 package com.dezdeqness.data.provider
 
+import com.dezdeqness.contract.anime.model.GenreEntity
 import com.dezdeqness.data.core.config.ConfigManager
 import com.dezdeqness.contract.anime.model.TypeEntity
+import com.dezdeqness.contract.settings.models.UserSelectedInterestsPreference
+import com.dezdeqness.contract.settings.repository.SettingsRepository
+import kotlinx.coroutines.runBlocking
 
 class HomeGenresProvider(
     private val configurationProvider: ConfigurationProvider,
     private val configManager: ConfigManager,
+    private val settingsRepository: SettingsRepository,
 ) {
 
     fun getHomeSectionGenresIds() =
         getHomeSectionGenres().map { it.numericId }
 
-    fun getHomeSectionGenres() =
-        configurationProvider
+    fun getHomeSectionGenres(): List<GenreEntity> {
+        val userSelected = runBlocking {
+            settingsRepository.getPreference(UserSelectedInterestsPreference)
+        }
+
+        val ids = if (userSelected.isNotEmpty()) {
+            userSelected.mapNotNull { it.toIntOrNull() }
+        } else {
+            configManager.homeGenresList
+        }
+
+        return configurationProvider
             .getListGenre()
             .filter { it.type == TypeEntity.ANIME }
-            .filter { item -> configManager.homeGenresList.contains(item.numericId) }
+            .filter { ids.contains(it.numericId) }
+    }
 
 }
