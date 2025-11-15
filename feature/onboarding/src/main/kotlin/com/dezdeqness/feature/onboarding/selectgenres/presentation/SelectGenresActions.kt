d226639fa7da87b3f8bd94750c2504c5f4d9d298
Danyl Brahin <inicerer141@gmail.com>
2025-11-15 16:02:56 +0200

    [DA-11] Add toolbar and localization

--- feature/onboarding/src/main/kotlin/com/dezdeqness/feature/onboarding/selectgenres/presentation/SelectGenresActions.kt
+++ feature/onboarding/src/main/kotlin/com/dezdeqness/feature/onboarding/selectgenres/presentation/SelectGenresActions.kt
@@ -1,6 +1,7 @@
 package com.dezdeqness.feature.onboarding.selectgenres.presentation
 
 interface SelectGenresActions {
+    fun onBackPressed()
     fun onGenreClicked(genreId: String)
     fun onSaveClicked()
 }
