5d5fc5ae8453ebe1fb922c2e2ee6985ba65abbd4
Danyl Brahin <inicerer141@gmail.com>
2026-01-17 19:44:48 +0200

    [DA-153] Prepare data & domain layers

---
+++ domain/src/main/java/com/dezdeqness/domain/usecases/GetPersonalListByStatusUseCase.kt
@@ -0,0 +1,40 @@
+package com.dezdeqness.domain.usecases
+
+import com.dezdeqness.contract.anime.model.UserRateEntity
+import com.dezdeqness.domain.repository.UserRatesRepository
+
+class GetPersonalListByStatusUseCase(
+    private val userRatesRepository: UserRatesRepository,
+) {
+    suspend operator fun invoke(page: Int, status: String): Result<PersonalListState> {
+        val result = userRatesRepository.getUserRates(
+            page = page,
+            limit = PAGE_SIZE,
+            status = status,
+        )
+        result.onFailure {
+            return Result.failure(it)
+        }
+        val list = result.getOrDefault(listOf())
+
+        val hasNextPage = list.size > PAGE_SIZE || list.isNotEmpty()
+
+        return Result.success(
+            PersonalListState(
+                list = list,
+                hasNextPage = hasNextPage,
+                currentPage = if (list.isEmpty()) page else page + 1,
+            )
+        )
+    }
+
+    data class PersonalListState(
+        val list: List<UserRateEntity> = listOf(),
+        val hasNextPage: Boolean = false,
+        val currentPage: Int = 0,
+    )
+
+    companion object {
+        private const val PAGE_SIZE = 50
+    }
+}
