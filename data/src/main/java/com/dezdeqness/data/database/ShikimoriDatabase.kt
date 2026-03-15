d8f7611e6136f25c4796bc21d0a6d0146da0c783
Danyl Brahin <inicerer141@gmail.com>
2026-03-15 12:50:34 +0200

    [DA-54] DB changes

--- data/src/main/java/com/dezdeqness/data/database/ShikimoriDatabase.kt
+++ data/src/main/java/com/dezdeqness/data/database/ShikimoriDatabase.kt
@@ -4,15 +4,22 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.dezdeqness.data.datasource.db.dao.AccountDao
+import com.dezdeqness.data.datasource.db.dao.AccountSessionDao
 import com.dezdeqness.data.datasource.db.dao.UserRatesDao
 import com.dezdeqness.data.model.db.AccountLocal
+import com.dezdeqness.data.model.db.AccountSessionLocal
 import com.dezdeqness.data.model.db.UserRateLocal
 
 @TypeConverters(StatusConverter::class, StatsConverter::class)
-@Database(entities = [AccountLocal::class, UserRateLocal::class], version = 1)
+@Database(
+    entities = [AccountLocal::class, UserRateLocal::class, AccountSessionLocal::class],
+    version = 2,
+)
 abstract class ShikimoriDatabase : RoomDatabase() {
 
     abstract fun accountDao(): AccountDao
+
+    abstract fun accountSessionDao(): AccountSessionDao
 
     abstract fun userRatesDao(): UserRatesDao
 
