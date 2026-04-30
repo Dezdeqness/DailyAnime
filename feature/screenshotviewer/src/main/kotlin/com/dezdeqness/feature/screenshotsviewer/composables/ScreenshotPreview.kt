2e3ef7012e7746c8b966006e7fae7842797a4c76
Danyl Brahin <inicerer141@gmail.com>
2026-04-30 16:31:07 +0300

    [DA-161] Screenshots preview, fix issue relate status protection view

---
+++ feature/screenshotviewer/src/main/kotlin/com/dezdeqness/feature/screenshotsviewer/composables/ScreenshotPreview.kt
@@ -0,0 +1,100 @@
+package com.dezdeqness.feature.screenshotsviewer.composables
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import com.dezdeqness.foundation.ui.views.image.AppImage
+import kotlinx.coroutines.launch
+
+@Composable
+fun ScreenshotPreview(
+    items: List<String>,
+    pagerState: PagerState,
+    isToolbarVisible: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val scope = rememberCoroutineScope()
+    val listState = rememberLazyListState()
+
+    LaunchedEffect(pagerState.currentPage, isToolbarVisible) {
+        val layoutInfo = listState.layoutInfo
+        val visibleItems = layoutInfo.visibleItemsInfo
+
+        val target = visibleItems.find { it.index == pagerState.currentPage }
+
+        if (target != null) {
+            val center = layoutInfo.viewportEndOffset / 2
+            val itemCenter = target.offset + target.size / 2
+            val scroll = itemCenter - center
+
+            listState.animateScrollBy(scroll.toFloat())
+        } else {
+            listState.animateScrollToItem(pagerState.currentPage)
+        }
+    }
+
+    AnimatedVisibility(
+        visible = isToolbarVisible,
+        enter = slideInVertically(
+            initialOffsetY = { it }
+        ) + fadeIn(),
+        exit = slideOutVertically(
+            targetOffsetY = { it }
+        ) + fadeOut(),
+        modifier = modifier,
+    ) {
+
+        LazyRow(
+            state = listState,
+            modifier = Modifier.fillMaxWidth().height(80.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            contentPadding = PaddingValues(horizontal = 12.dp)
+        ) {
+            itemsIndexed(items) { index, item ->
+                val isSelected = pagerState.currentPage == index
+
+                AppImage(
+                    data = item,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .border(
+                            width = if (isSelected) 1.dp else 0.dp,
+                            color = if (isSelected) Color.White else Color.Transparent,
+                            shape = RoundedCornerShape(8.dp)
+                        )
+                        .clickable {
+                            scope.launch {
+                                pagerState.animateScrollToPage(index)
+                            }
+                        }
+                )
+            }
+        }
+    }
+}
