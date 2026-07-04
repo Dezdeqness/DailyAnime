94e4ddb8f9cd7ffdb5ee3fe7ea6bd1f1dea36b1d
Danyl Brahin <inicerer141@gmail.com>
2026-07-04 14:56:38 +0300

    [DA-174] Clean up old code

--- app/src/main/java/com/dezdeqness/presentation/models/ChronologyUiModel.kt
+++
@@ -1,8 +0,0 @@
-package com.dezdeqness.presentation.models
-
-data class ChronologyUiModel(
-    val id: Long,
-    val name: String,
-    val imageUrl: String,
-    val briefInfo: String,
-) : AdapterItem()
--- app/src/main/java/com/dezdeqness/presentation/models/StatsHeaderUiModel.kt
+++
@@ -1,5 +0,0 @@
-package com.dezdeqness.presentation.models
-
-data class StatsHeaderUiModel(
-    val header: String,
-) : AdapterItem()
