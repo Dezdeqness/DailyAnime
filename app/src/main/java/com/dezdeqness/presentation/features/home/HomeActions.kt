221e9bee8a1dd8cedd9e98e26539e3b8791ddbdd
Danyl Brahin <inicerer141@gmail.com>
2026-07-14 22:09:13 +0300

    [DA-172] Clean up

--- app/src/main/java/com/dezdeqness/presentation/features/home/HomeActions.kt
+++
@@ -1,8 +0,0 @@
-package com.dezdeqness.presentation.features.home
-
-import com.dezdeqness.presentation.action.Action
-
-interface HomeActions {
-    fun onInitialLoad()
-    fun onActionReceived(action: Action)
-}
--- app/src/main/java/com/dezdeqness/presentation/features/home/model/SectionStatus.kt
+++
@@ -1,8 +0,0 @@
-package com.dezdeqness.presentation.features.home.model
-
-enum class SectionStatus {
-    Initial,
-    Loading,
-    Error,
-    Loaded,
-}
